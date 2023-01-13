80e6bcbc83cc9583c0f6428b9699a5930d81ea77
Angelaz9 <64970309+Angelaz9@users.noreply.github.com>
2023-01-13 09:53:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     basic.showIcon(IconNames.Tortoise)
 })
-basic.showIcon(IconNames.Heart)
